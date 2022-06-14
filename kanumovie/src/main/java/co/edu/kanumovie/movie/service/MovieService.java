4c35a8e5543726a5c82b0c73e748434ffe6b7eb8
Tessa1217 <kwonyuj1996@gmail.com>
2022-06-14 20:05:32 +0900

    yujin commit - fix movie

--- kanumovie/src/main/java/co/edu/kanumovie/movie/service/MovieService.java
+++ kanumovie/src/main/java/co/edu/kanumovie/movie/service/MovieService.java
@@ -2,11 +2,13 @@ package co.edu.kanumovie.movie.service;
 
 import java.util.List;
 
+import co.edu.kanumovie.genre.vo.GenreVO;
 import co.edu.kanumovie.movie.vo.MovieVO;
 
 public interface MovieService {
 	
 	public List<MovieVO> movieSelectList();
+	public List<MovieVO> movieSelectGenreList(GenreVO vo);
 	public MovieVO movieSelect(MovieVO vo);
 	public int movieInsert(MovieVO vo); 
 	public int movieUpdate(MovieVO vo);
