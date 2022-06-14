2fbdaf07a26beafc891e4e16532ae19f0613d2a8
hoo788 <hoo788@gmail.com>
2022-06-14 11:08:14 +0900

    gyuchan: 0614: logout.java

---
+++ kanumovie/src/main/java/co/edu/kanumovie/user/command/Logout.java
@@ -0,0 +1,20 @@
+package co.edu.kanumovie.user.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import co.edu.kanumovie.common.Command;
+
+public class Logout implements Command {
+
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		// 세션 로그아웃
+		HttpSession session = request.getSession();
+		session.invalidate(); // 세션객체를 완전히 삭제.
+		request.setAttribute("message", "logout");
+		return "home/home";
+	}
+
+}
