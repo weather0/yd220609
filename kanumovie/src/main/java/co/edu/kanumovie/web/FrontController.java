313ae8cf74f7b63a37d3042ba35efa91887bc44c
weather0 <af1291309@gmail.com>
2022-06-16 17:50:46 +0900

    Juyup: Update MovieInfo / Add Likes

--- kanumovie/src/main/java/co/edu/kanumovie/web/FrontController.java
+++ kanumovie/src/main/java/co/edu/kanumovie/web/FrontController.java
@@ -25,6 +25,9 @@ import co.edu.kanumovie.comment.command.CommentInsert;
 import co.edu.kanumovie.common.Command;
 import co.edu.kanumovie.genre.command.GenreSelectList;
 import co.edu.kanumovie.home.command.Home;
+import co.edu.kanumovie.likes.command.Likes;
+import co.edu.kanumovie.likes.command.LikesDelete;
+import co.edu.kanumovie.likes.command.LikesSelectList;
 import co.edu.kanumovie.movie.command.MovieInfo;
 import co.edu.kanumovie.movie.command.MovieInfoghtest;
 import co.edu.kanumovie.movie.command.MoviePlay;
@@ -84,6 +87,9 @@ public class FrontController extends HttpServlet {
 		map.put("/movieInfoghtest.do", new MovieInfoghtest());
 		map.put("/moviePlay.do", new MoviePlay());
 		map.put("/movieSearch.do", new MovieSearch());
+		map.put("/likes.do", new Likes());
+		map.put("/likesDelete.do", new LikesDelete());
+		map.put("/likesSelectList.do", new LikesSelectList());
 	}
 
 	@Override
